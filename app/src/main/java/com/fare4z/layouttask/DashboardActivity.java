2d99c2c56cd2280894db5f08ba68f230253237f2
fareez <fareez@psp.edu.my>
2024-03-02 23:53:04 +0800

    update

---
+++ app/src/main/java/com/fare4z/layouttask/DashboardActivity.java
@@ -0,0 +1,14 @@
+package com.fare4z.layouttask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
